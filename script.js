55dff9298f6281f7aa6d704f04c014cf505b28b3
Nikolaj Nyboe <nikolaj@nyboe.dk>
2017-11-07 12:53:44 +0100

    Login mostly done

--- script.js
+++ script.js
@@ -8,15 +8,26 @@ var btnCreateUser = document.querySelector("#btnCreateUser");
 var btnSignup = document.querySelector("#btnSignup");
 var btnGoToLogin = document.querySelector("#btnGoToLogin");
 
+var currentDirectory = window.location.pathname.split('/').slice(0, -1).join('/')
+
 document.addEventListener("click", e => {
     switch (e.target) {
+        case btnLogin:
+            console.log("btnLogin")
+            window.location.href = currentDirectory+"/dashboard.html";
+            break;
         case btnCreateUser:
             console.log("btnCreateUser");
+            formLogin.classList.add("hide");
+            formCreateUser.classList.remove("hide");
+            break;
+        case btnSignup:
+            console.log("btnSignup")
             break;
         case btnGoToLogin:
             console.log("btnGoToLogin");
+            formLogin.classList.remove("hide");
+            formCreateUser.classList.add("hide");
             break;
-        default:
-            console.log('Sorry');
-    }
+    };
 });
